77aaa7f966f2a4f460d52543b697fd091a2bbc54
joanna <okokis101@gmail.com>
2020-07-08 19:12:07 +0800

    temp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { stationList } from './station-list.const';
+import { Message } from "./message";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-study';
+  // 宣告 元件的data資料
+  list = stationList;
+  name = '';
+  content = '';
+
+  data: string[];
+
+ // xxx: Message[] = [];
+
+  addMessage(): void {
+
+    // 防呆，避免名稱或內容是空值時也可以留言
+    if (!this.name.trim() || !this.content.trim()) {
+      return;
+    }
+
+    // 用名稱跟內容產生一個留言的資料物件
+    //const message = new Message(this.name, this.content);
+
+    // 將留言的資料物件放進容器裡
+    //this.xxx.push({message});
+    this.data.push(this.name);
+    // 清空內容
+    this.content = '';
+
+  }
 }
---
+++ src/app/station-list.const.ts
@@ -0,0 +1,178 @@
+export const stationList = [
+  {
+    'Station': '三和國中站',
+    'Destination': '蘆洲站',
+    'UpdateTime': '2020-07-07T11:40:10.533'
+  },
+  {
+    'Station': '大安站',
+    'Destination': '南港展覽館站',
+    'UpdateTime': '2020-07-07T11:39:34'
+  },
+  {
+    'Station': '大坪林站',
+    'Destination': '新店站',
+    'UpdateTime': '2020-07-07T11:39:52.34'
+  },
+  {
+    'Station': '中山國小站',
+    'Destination': '蘆洲站',
+    'UpdateTime': '2020-07-07T11:40:10.533'
+  },
+  {
+    'Station': '中正紀念堂站',
+    'Destination': '松山站',
+    'UpdateTime': '2020-07-07T11:40:02.843'
+  },
+  {
+    'Station': '中正紀念堂站',
+    'Destination': '新店站',
+    'UpdateTime': '2020-07-07T11:40:02.843'
+  },
+  {
+    'Station': '內湖站',
+    'Destination': '南港展覽館站',
+    'UpdateTime': '2020-07-07T11:39:34'
+  },
+  {
+    'Station': '內湖站',
+    'Destination': '動物園站',
+    'UpdateTime': '2020-07-07T11:39:34'
+  },
+  {
+    'Station': '六張犁站',
+    'Destination': '南港展覽館站',
+    'UpdateTime': '2020-07-07T11:39:34'
+  },
+  {
+    'Station': '公館站',
+    'Destination': '新店站',
+    'UpdateTime': '2020-07-07T11:39:42.297'
+  },
+  {
+    'Station': '古亭站',
+    'Destination': '迴龍站',
+    'UpdateTime': '2020-07-07T11:40:09.57'
+  },
+  {
+    'Station': '台大醫院站',
+    'Destination': '大安站',
+    'UpdateTime': '2020-07-07T11:40:05.233'
+  },
+  {
+    'Station': '台大醫院站',
+    'Destination': '北投站',
+    'UpdateTime': '2020-07-07T11:40:05.233'
+  },
+  {
+    'Station': '台北小巨蛋站',
+    'Destination': '松山站',
+    'UpdateTime': '2020-07-07T11:39:52.34'
+  },
+  {
+    'Station': '台電大樓站',
+    'Destination': '松山站',
+    'UpdateTime': '2020-07-07T11:39:42.297'
+  },
+  {
+    'Station': '西門站',
+    'Destination': '松山站',
+    'UpdateTime': '2020-07-07T11:39:52.34'
+  },
+  {
+    'Station': '辛亥站',
+    'Destination': '南港展覽館站',
+    'UpdateTime': '2020-07-07T11:39:34'
+  },
+  {
+    'Station': '府中站',
+    'Destination': '亞東醫院站',
+    'UpdateTime': '2020-07-07T11:39:56.89'
+  },
+  {
+    'Station': '忠孝復興站',
+    'Destination': '頂埔站',
+    'UpdateTime': '2020-07-07T11:39:56.89'
+  },
+  {
+    'Station': '忠義站',
+    'Destination': '淡水站',
+    'UpdateTime': '2020-07-07T11:40:05.233'
+  },
+  {
+    'Station': '昆陽站',
+    'Destination': '南港展覽館站',
+    'UpdateTime': '2020-07-07T11:40:07.34'
+  },
+  {
+    'Station': '明德站',
+    'Destination': '北投站',
+    'UpdateTime': '2020-07-07T11:40:05.233'
+  },
+  {
+    'Station': '東門站',
+    'Destination': '蘆洲站',
+    'UpdateTime': '2020-07-07T11:39:50.13'
+  },
+  {
+    'Station': '松山機場站',
+    'Destination': '南港展覽館站',
+    'UpdateTime': '2020-07-07T11:39:34'
+  },
+  {
+    'Station': '松山機場站',
+    'Destination': '動物園站',
+    'UpdateTime': '2020-07-07T11:39:34'
+  },
+  {
+    'Station': '松江南京站',
+    'Destination': '南勢角站',
+    'UpdateTime': '2020-07-07T11:40:10.533'
+  },
+  {
+    'Station': '南勢角站',
+    'Destination': '迴龍站',
+    'UpdateTime': '2020-07-07T11:40:09.57'
+  },
+  {
+    'Station': '紅樹林站',
+    'Destination': '象山站',
+    'UpdateTime': '2020-07-07T11:40:05.233'
+  },
+  {
+    'Station': '唭哩岸站',
+    'Destination': '象山站',
+    'UpdateTime': '2020-07-07T11:40:05.233'
+  },
+  {
+    'Station': '善導寺站',
+    'Destination': '亞東醫院站',
+    'UpdateTime': '2020-07-07T11:40:07.34'
+  },
+  {
+    'Station': '圓山站',
+    'Destination': '北投站',
+    'UpdateTime': '2020-07-07T11:40:05.233'
+  },
+  {
+    'Station': '萬芳社區站',
+    'Destination': '南港展覽館站',
+    'UpdateTime': '2020-07-07T11:39:34'
+  },
+  {
+    'Station': '萬隆站',
+    'Destination': '松山站',
+    'UpdateTime': '2020-07-07T11:39:52.34'
+  },
+  {
+    'Station': '葫洲站',
+    'Destination': '南港展覽館站',
+    'UpdateTime': '2020-07-07T11:39:34'
+  },
+  {
+    'Station': '葫洲站',
+    'Destination': '動物園站',
+    'UpdateTime': '2020-07-07T11:39:34'
+  }
+
+]
