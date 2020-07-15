f2cd56cc2228d21f8a3c4a2b0dee105e42c9f660
joanna <okokis101@gmail.com>
2020-07-15 11:06:50 +0800

    kendo-grid分頁載入

--- src/app/kendo-grid/kendo-grid.component.ts
+++ src/app/kendo-grid/kendo-grid.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { bookData } from "../book-data.const";
 
+import { GridDataResult, PageChangeEvent } from '@progress/kendo-angular-grid';
+
 @Component({
   selector: 'app-kendo-grid',
   templateUrl: './kendo-grid.component.html',
@@ -9,13 +11,31 @@ import { bookData } from "../book-data.const";
 
 })
 export class KendoGridComponent implements OnInit {
+  public gridView: GridDataResult;
+  public pageSize = 10;
+  public skip = 0;
+  private data: Object[];
 
-  constructor() { }
+  constructor() {
+    this.loadItems();
+   }
 
   ngOnInit(): void {
   }
 
   public gridData: any[] = bookGidData;
+
+  public pageChange(event: PageChangeEvent): void {
+    this.skip = event.skip;
+    this.loadItems();
+  }
+
+  private loadItems(): void {
+      this.gridView = {
+          data: this.gridData.slice(this.skip, this.skip + this.pageSize),
+          total: this.gridData.length
+      };
+  }
 }
 
 const bookGidData = bookData;
