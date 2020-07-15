cfeaf48d6fbd1356912f182ee81d8e6c4cf3a3a3
joanna <okokis101@gmail.com>
2020-07-15 10:00:37 +0800

    add kendo Grid package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { BookFormModule } from "./book-form/book-form.module";
 import { BookTableModule } from "./book-table/book-table.module";
+import { GridModule } from '@progress/kendo-angular-grid';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
@@ -14,7 +18,9 @@ import { BookTableModule } from "./book-table/book-table.module";
     BrowserModule,
     FormsModule,
     BookFormModule,
-    BookTableModule
+    BookTableModule,
+    GridModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
