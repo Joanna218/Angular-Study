81e509ca9ace0fc96ee7d80da9a249db41f43045
joanna <okokis101@gmail.com>
2020-07-17 13:54:26 +0800

    整理命名、function的公開、私有

--- src/app/kendo-grid/kendo-grid.component.ts
+++ src/app/kendo-grid/kendo-grid.component.ts
@@ -21,19 +21,15 @@ interface dropDown {
 
 })
 export class KendoGridComponent implements OnInit {
+  // grid雙向綁定資料
   public gridView: GridDataResult;
   public pageSize = 10;
   public skip = 0;
-  private data: Object[];
-
-  constructor(private kendoGridService: KendoGridService) {
-    this.loadItems();
-   }
-
-  ngOnInit(): void {
-  }
-
-  // dropDown
+  // bookDialog預設為關著
+  public bookDialogMode = false;
+  // 判別現在的操作模式(新增:true or 修改:false)
+  public isAddBookMode = true;
+  // 書籍類別dropDownList
   public bookCategoryItems: Array<dropDown> = [
     { text: "資料庫", value: "database" },
     { text: "網際網路", value: "internet" },
@@ -41,24 +37,33 @@ export class KendoGridComponent implements OnInit {
     { text: "應用系統整合", value: "system" },
     { text: "語言", value: "language" }
   ];
-
-  public selectedItem: dropDown = this.bookCategoryItems[0];
-
-  public selectionChange(data: any): void {
+  // 類別下拉選單被選到的整包資料，預設為第一筆資料
+  public selectedBookCategoryItem: dropDown = this.bookCategoryItems[0];
+  // 類別下選單改變
+  public changeBookCategory(data: any): void {
     this.bookCategory = data.text;
   }
-
+  // grid初始資料
   public gridData: any[] = this.kendoGridService.initData();
 
-
-  // form
+  // 書籍資料雙向綁定
   bookName: string;
   bookCategory: string;
   bookAuthor: string;
   // datePicker
   bookBoughtDate: Date = new Date();
+  // tslint:disable-next-line: no-trailing-whitespace
 
-  addBook(): void {
+  constructor(private kendoGridService: KendoGridService) {
+    this.loadBookData();
+   }
+
+  ngOnInit(): void {
+  }
+
+
+  // 視窗中新增書籍按鈕
+  public addBook(): void {
     const newBookId = this.kendoGridService.getMaxBookId() + 1;
     this.kendoGridService.setMaxBookId(newBookId);
     const addBook = new Book(newBookId, this.bookName, this.bookCategory, this.bookAuthor, this.bookBoughtDate);
@@ -67,82 +72,67 @@ export class KendoGridComponent implements OnInit {
     // 新增成功提示
     // 清空欄位
     this.resetAddBookFrom();
-    this.loadItems();
+    this.loadBookData();
   }
-
-  getBookList(): Book[] {
+  // 拿到全部的書籍資料
+  public getBookList(): Book[] {
     return this.kendoGridService.getBookList();
   }
 
-  deleteBook(idx: number): void {
-    this.kendoGridService.deleteBook(idx);
-  }
-
-  // grid Component
+  // grid切換分頁
   public pageChange(event: PageChangeEvent): void {
     this.skip = event.skip;
-    this.loadItems();
+    this.loadBookData();
   }
 
-  // 重新讀取分頁資料
-  private loadItems(): void {
+  // 讀取分頁資料至gridView
+  private loadBookData(): void {
       this.gridView = {
           data: this.gridData.slice(this.skip, this.skip + this.pageSize),
           total: this.gridData.length
       };
   }
-
+  // 刪除書籍Handler
   public removeHandler(deleteData: any): void {
     // 找到該筆資料的index
     const deleteDataItemIdx = this.gridData.indexOf(deleteData.dataItem);
     // 刪除該筆資料
     this.gridData.splice(deleteDataItemIdx, 1);
     // gridView reload
-    this.loadItems();
+    this.loadBookData();
   }
-
+  // 編輯書籍Handler
   public editHandler(editData: any): void {
     const editBookData = editData.dataItem;
     this.bookName = editBookData.BookName;
     this.bookAuthor = editBookData.BookAuthor;
     const bookCategoryData = this.bookCategoryItems.filter(item => item.text === editBookData.BookCategory);
-    this.selectedItem = bookCategoryData[0];
-    // 處理購買日期，從字串轉成date綁上控制向
-    const replaceDateString = editBookData.BookBoughtDate.replace(/-/g, ',');
-    // const [yyyy, MM, dd] = replaceDateString.split(',');
-    // this.bookBoughtDate = new Date(parseInt(yyyy, 10), parseInt(MM, 10), parseInt(dd, 10));
-    // this.bookBoughtDate = editBookData.BookBoughtDate;
+    this.selectedBookCategoryItem = bookCategoryData[0];
     this.bookBoughtDate = new Date(editBookData.BookBoughtDate);
-    this.toggle = true;
-    this.open();
+    this.isAddBookMode = false;
+    this.openBookDialog();
   }
 
-  // addBookWindow
-  public opened = false;
-  public toggle = false;
 
-  public close(): void {
-    this.opened = false;
-  }
-
-  public open(): void {
-    this.opened = true;
-  }
-
-  // 清空新增書籍欄位
-  resetAddBookFrom(): void {
+  // 清空視窗中所有欄位
+  private resetAddBookFrom(): void {
     this.bookName = '';
     this.bookAuthor = '';
     this.bookCategory = '';
-    this.selectedItem = this.bookCategoryItems[0];
+    this.selectedBookCategoryItem = this.bookCategoryItems[0];
     this.bookBoughtDate = new Date();
   }
-
-  addBookDialog(): void {
+  // 新增書籍視窗
+  public openAddBookDialog(): void {
     this.resetAddBookFrom();
-    this.toggle = false;
-    this.open();
+    this.isAddBookMode = true;
+    this.openBookDialog();
+  }
+
+  public closeBookDialog(): void {
+    this.bookDialogMode = false;
+  }
+  private openBookDialog(): void {
+    this.bookDialogMode = true;
   }
 }
-
-// const bookGidData = bookData;
